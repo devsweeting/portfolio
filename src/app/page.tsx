fb53f89c3ab3a8fd44ab02bc5118a8535163d95e
Devin Sweeting <devinsweeting@gmail.com>
2025-01-03 13:28:07 -0800

    feat: Save local blob controls in local storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,53 +6,13 @@ import Title from "@/components/title";
 import Spheres from "./three";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import Socials from "@/components/Socials";
-import { Leva, useControls } from "leva";
+import { Leva } from "leva";
 import useIsMobile from "./utils/useIsMobile";
+import { useLocalBlobControls } from "./utils/useLocalBlobControls";
 
 export default function Home() {
-  const { image, outlines, sphereColor, background, brightness } = useControls({
-    image: {
-      label: "Image",
-      options: {
-        Blackberry: "/images/blackberry.png",
-        Cross: "/images/cross.jpg",
-        "My Cat": "/images/mycat.png",
-      },
-      value: "/images/cross.jpg",
-    },
-    outlines: { label: "Outlines", value: 0.0, step: 0.01, min: 0, max: 0.05 },
-    sphereColor: {
-      label: "Color",
-      options: {
-        White: "#ffffff",
-        Red: "#EF1F14",
-        Green: "#2FF44F",
-        Blue: "#2048DB",
-        Yellow: "#FAF436",
-        Purple: "#D212E1",
-      },
-      value: "#ffffff",
-    },
-    background: {
-      label: "Background",
-      options: {
-        White: "#dfdfdf",
-        Red: "#F0605D",
-        Green: "#73F587",
-        Blue: "#7991DB",
-        Yellow: "#FAF24F",
-        Purple: "#D148E1",
-      },
-      value: "#dfdfdf",
-    },
-    brightness: {
-      label: "Brightness",
-      value: 0.5,
-      step: 0.5,
-      min: 0,
-      max: 2.5,
-    },
-  });
+  const { image, outlines, color, background, brightness } =
+    useLocalBlobControls();
 
   const isMobile = useIsMobile();
 
@@ -68,7 +28,7 @@ export default function Home() {
           <Spheres
             background={background}
             brightness={brightness}
-            sphereColor={sphereColor}
+            sphereColor={color}
             image={image}
             outlines={outlines}
             // isMobile={isMobile}
---
+++ src/app/utils/useLocalBlobControls.ts
@@ -0,0 +1,116 @@
+"use client";
+import { Leva, useControls } from "leva";
+
+let controlDefinitions = {
+  image: {
+    label: "Image",
+    options: {
+      Blackberry: "/images/blackberry.png",
+      Cross: "/images/cross.jpg",
+      "My Cat": "/images/mycat.png",
+    },
+    value: "/images/cross.jpg",
+    onChange: (v) => {
+      localStorage.setItem("Image", JSON.stringify(v));
+    },
+    transient: false,
+  },
+  outlines: {
+    label: "Outlines",
+    value: 0.0,
+    step: 0.01,
+    min: 0,
+    max: 0.05,
+    onChange: (v) => {
+      localStorage.setItem("Outlines", JSON.stringify(v));
+    },
+    transient: false,
+  },
+  color: {
+    label: "Color",
+    options: {
+      White: "#ffffff",
+      Red: "#EF1F14",
+      Green: "#2FF44F",
+      Blue: "#2048DB",
+      Yellow: "#FAF436",
+      Purple: "#D212E1",
+    },
+    value: "#ffffff",
+    onChange: (v) => {
+      localStorage.setItem("Color", JSON.stringify(v));
+    },
+    transient: false,
+  },
+  background: {
+    label: "Background",
+    options: {
+      White: "#dfdfdf",
+      Red: "#F0605D",
+      Green: "#73F587",
+      Blue: "#7991DB",
+      Yellow: "#FAF24F",
+      Purple: "#D148E1",
+    },
+    value: "#dfdfdf",
+    onChange: (v) => {
+      localStorage.setItem("Background", JSON.stringify(v));
+    },
+    transient: false,
+  },
+  brightness: {
+    label: "Brightness",
+    value: 0.5,
+    step: 0.5,
+    min: 0,
+    max: 2.5,
+    onChange: (v) => {
+      localStorage.setItem("Brightness", JSON.stringify(v));
+    },
+    transient: false,
+  },
+};
+
+export const useLocalBlobControls = () => {
+  // Check if each label exists in localStorage
+  Object.values(controlDefinitions).forEach((control) => {
+    const controlValueInStorage = localStorage.getItem(control.label);
+    const existsInStorage = controlValueInStorage !== null;
+    console.log(
+      `Label "${control.label}" exists in localStorage: ${existsInStorage} -- ${controlValueInStorage}`
+    );
+    if (!existsInStorage) {
+      localStorage.setItem(control.label, JSON.stringify(control.value));
+    } else {
+      // Set localStorage value to controlDefinitions
+      const definition = controlDefinitions[control.label.toLowerCase()];
+      if (definition) {
+        try {
+          const parsedValue = JSON.parse(controlValueInStorage);
+          // Ensure valid value type for outlines
+          if (
+            control.label === "Outlines" ||
+            (control.label === "Brightness" && typeof parsedValue !== "number")
+          ) {
+            throw new Error(
+              `Invalid value type for "${control.label}". Expected a number.`
+            );
+          }
+
+          definition.value = parsedValue;
+        } catch (error) {
+          console.error(`Error parsing value for "${control.label}":`, error);
+        }
+      } else {
+        console.error(
+          `Definition not found for label "${control.label.toLowerCase()}".`
+        );
+      }
+    }
+  });
+
+  const { image, outlines, color, background, brightness } =
+    useControls(controlDefinitions);
+
+  return { image, outlines, color, background, brightness };
+};
